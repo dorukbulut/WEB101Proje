8f44a63b731f2a4074db2610595668e662a769a0
Doruk Bulut <dorukbulutinfo@gmail.com>
2022-11-09 01:14:27 +0300

    Create Mentee Mentor Association

--- README.md
+++
@@ -1,3 +0,0 @@
-# WEB101Proje
-
-##GSU 2021/2022- Web Programlamaya Giriş Dersi kapmasında geliştirilen e-mentörlük platformu##
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from  "express";
-import menteeRoutes from './mentee.routes.js'
-const router = express.Router();
-
-router.use("/mentee", menteeRoutes)
-
-
-
-export default router;
