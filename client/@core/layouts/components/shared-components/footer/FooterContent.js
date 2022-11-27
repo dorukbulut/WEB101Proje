114319e2a14a6adc6720bcc3ef10511c57fe8ae5
Doruk Bulut <dorukbulutinfo@gmail.com>
2022-11-27 17:51:10 +0300

    Vize

---
+++ client/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -0,0 +1,52 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+const FooterContent = () => {
+  // ** Var
+  
+
+  return (
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
+      <Typography sx={{ mr: 2 }}>
+        
+        <Box component='span' sx={{ color: 'error.main' }}>
+          
+        </Box>
+        
+        <Link target='_blank' href='https://themeselection.com/'>
+          
+        </Link>
+      </Typography>
+      {false ? null : (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
+          <Link
+            target='_blank'
+            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
+          >
+            
+          </Link>
+          <Link target='_blank' href='https://themeselection.com/'>
+            
+          </Link>
+          <Link
+            target='_blank'
+            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
+          >
+            
+          </Link>
+          <Link
+            target='_blank'
+            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
+          >
+            
+          </Link>
+        </Box>
+      )}
+    </Box>
+  )
+}
+
+export default FooterContent
--- client/pages/mentee-dashboard/index.js
+++ client/pages/mentee-dashboard/index.js
@@ -1,47 +1,98 @@
 import axios from "axios";
 import {useRouter} from "next/router";
-import MenteeNavbar from "../../components/ui/MenteeNavbar";
-import EventsPreview from "../../components/ui/MenteeEventsPreview";
-import TasksPreview from "../../components/ui/MentorTasksPreview";
-import MentorPreview from "../../components/ui/MenteeMentorPreview";
+import Head from 'next/head'
+import { Router } from 'next/router'
 
-const MenteeDashBoard = (props) => {
-    const router = useRouter();
-    const handleLogOut = async (e) => {
-        e.preventDefault();
+// ** Loader Import
+import NProgress from 'nprogress'
 
-        const res = await axios.post("http://localhost:3001/mentee/logout",props.authToken,{withCredentials: true});
+// ** Emotion Imports
+import { CacheProvider } from '@emotion/react'
 
-        if(res.status === 201) {
-            router.push("/");
-        }
+// ** Config Imports
+import themeConfig from '../../configs/themeConfig'
 
-        else {
-            console.log(res)
-        }
-    }
-    return (
-        <div className="grid grid-cols-6 gap-4 bg-main-color h-screen">
-            <div className=" col-span-1 ">
-                <MenteeNavbar />
-            </div>
-            
-            <div className="col-span-5 p-10 flex flex-col">
-                <div className="flex">
-                    <MentorPreview />
-                    <TasksPreview />
-                </div>
-            
-                <div>
-                    <EventsPreview />
-                </div>
-               
-                
-            </div>
-        </div>
-    );
+// ** Component Imports
+import UserLayout from '../../layouts/UserLayout'
+import ThemeComponent from '../../@core/theme/ThemeComponent'
+
+// ** Contexts
+import { SettingsConsumer, SettingsProvider } from '../../@core/context/settingsContext'
+
+// ** Utils Imports
+import { createEmotionCache } from '../../@core/utils/create-emotion-cache'
+
+// ** React Perfect Scrollbar Style
+import 'react-perfect-scrollbar/dist/css/styles.css'
+
+
+
+const clientSideEmotionCache = createEmotionCache()
+
+// ** Pace Loader
+if (themeConfig.routingLoader) {
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+  Router.events.on('routeChangeError', () => {
+    NProgress.done()
+  })
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
 }
 
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+
+
+
+
+// ** Styled Component Import
+import ApexChartWrapper from '../../@core/styles/libs/react-apexcharts'
+
+// ** Demo Components Imports
+import Table from '../../views/dashboard/Table'
+import Trophy from '../../views/dashboard/Trophy'
+
+import StatisticsCard from '../../views/dashboard/StatisticsCard'
+import WeeklyOverview from '../../views/dashboard/WeeklyOverview'
+
+import MenteeNavbar from "../../components/ui/MenteeNavbar";
+const Dashboard = () => {
+    
+
+  // Variables
+  
+  return (
+    <div className="grid grid-cols-5">
+        <div className="col-span-1">
+            <MenteeNavbar />
+        </div>
+        
+        <div className="flex flex-col col-span-4 p-20 space-y-10">
+            <div className="flex space-x-10">
+                <Trophy />
+                <div className="w-1/2">
+                    <StatisticsCard />
+                </div>
+            </div>
+            <div className="">
+                <div>
+                    <Table />
+                </div>
+            </div>
+            
+        </div>
+        
+    </div>
+    
+
+        
+     
+   
+  )
+}
 export async function getServerSideProps(context) {
     const cookie = context.req.cookies
 
@@ -67,4 +118,8 @@ export async function getServerSideProps(context) {
 }
 
 
-export default MenteeDashBoard;
+
+
+
+
+export default Dashboard;
